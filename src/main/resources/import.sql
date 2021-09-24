5cad0b02e20a338877aef569880135e1d4cb94eb
윤수연 <syyun@dtech.ai>
2021-09-24 13:43:00 +0900

    db 관련 설정 추가 및 수정

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO member (created_at, updated_at, name, password, role, user_id) VALUES (now(), now(), '관리자', '$2a$10$.RujS8nz2WEwv/Qxob/zIO5WA8dpF0KWSUMpF2vRGn5g91Vb9./l6', 'ADMIN', 'admin')
